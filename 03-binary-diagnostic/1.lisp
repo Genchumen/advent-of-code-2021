9e511fbe1ab6f2fe1ee65cce61f156c9fde4f646
Sawyer Shepherd <contact@sawyershepherd.org>
2022-09-14 14:20:28 -0400

    Add question 3

---
+++ 03-binary-diagnostic/1.lisp
@@ -0,0 +1,24 @@
+(defun mapindex (func lists)
+  )
+
+(defun power-consumption (x)
+  (* (gamma-rate x) (epsilon-rate x)))
+
+(defun gamma-rate (x)
+  (reduce #'+ ))
+
+(defun epsilon-rate (x))
+
+(power-consumption 
+((0 0 1 0 0)
+(1 1 1 1 0)
+(1 0 1 1 0)
+(1 0 1 1 1)
+(1 0 1 0 1)
+(0 1 1 1 1)
+(0 0 1 1 1)
+(1 1 1 0 0)
+(1 0 0 0 0)
+(1 1 0 0 1)
+(0 0 0 1 0)
+(0 1 0 1 0))
